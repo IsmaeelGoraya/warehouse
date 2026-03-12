e697768b4c044d7e3058cbacc6093b0117c92b0a
Ismaeel Shujaat <ismaeel.shujaat@tinash.com>
2026-03-12 12:59:36 +0500

    Create products module and moves entity to respective folder.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SuppliersModule } from './suppliers/suppliers.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [SuppliersModule],
+  imports: [SuppliersModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
