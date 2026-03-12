aca826a7a54fa66cc868591021e065dcd447c975
Ismaeel Shujaat <ismaeel.shujaat@tinash.com>
2026-03-12 13:08:17 +0500

    Create products module and moves respective entities in it.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SuppliersModule } from './suppliers/suppliers.module';
+import { ProductsModule } from './products/products.module';
+import { PurchaseOrdersModule } from './purchase-orders/purchase-orders.module';
 
 @Module({
-  imports: [SuppliersModule],
+  imports: [SuppliersModule, ProductsModule, PurchaseOrdersModule],
   controllers: [AppController],
   providers: [AppService],
 })
